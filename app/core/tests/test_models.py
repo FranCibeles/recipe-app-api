6a616aefc2ad01940b80fe570efb4f6e5b2f5c11
Francisco Cisneros <francisco.cisnerosanton@swisscom.com>
2023-09-07 15:39:24 +0200

    Revert "Revert "User Endpoint finished""
    This reverts commit e38b3562ebe285514cceedbb965093d351e2daff.

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,9 @@ Tests for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from decimal import Decimal
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -49,3 +52,24 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """ Test create recipe is sucessfull """
+
+        # Create a user to whom is going to be the recipe assigned
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123'
+        )
+
+        # Create a new recipe
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description'
+        )
+
+        #
+        self.assertEqual(str(recipe), recipe.title)
